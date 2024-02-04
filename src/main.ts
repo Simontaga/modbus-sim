7d51012b19c99f462c26e384a25b3cdc18aa8bd3
Simon Törnlund <simon.tornlund@ecsolutions.se>
2024-02-04 20:01:27 +0100

    Remove more chart refs and update screenshot

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
-import VueApexCharts from "vue3-apexcharts";
-
 
 const app = createApp(App);
-app.use(VueApexCharts);
 app.mount("#app");
